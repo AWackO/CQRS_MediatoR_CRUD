ae7054de2b21b408cd3699f4909603125cdebdee
Aleksa <aleksa.milenkovic@atessoft.rs>
2024-03-20 12:19:01 +0100

    using in memory data now

--- CQRS_MediatoR_CRUD/Program.cs
+++ CQRS_MediatoR_CRUD/Program.cs
@@ -1,8 +1,6 @@
-using CQRS_MediatorR_Library.DbData;
 using CQRS_MediatorR_Library.Mappings;
 using CQRS_MediatorR_Library.Queries;
 using CQRS_MediatorR_Library.Repositories;
-using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,16 +13,16 @@ builder.Services.AddControllers()
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IGroceryRepository, InMemoryGroceryRepository>();
-builder.Services.AddScoped<IGroceryRepository, GroceryRepository>();
+//builder.Services.AddScoped<IGroceryRepository, GroceryRepository>();
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(GetGroceryListQuery).Assembly));
-builder.Services.AddScoped<IShoppingBagRepository, ShoppingBagRepository>();
+//builder.Services.AddScoped<IShoppingBagRepository, ShoppingBagRepository>();
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<DataContext>(options =>
-{
-    options.UseSqlServer(connectionString, b => b.MigrationsAssembly("CQRS_MediatoR_CRUD"));
-});
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+//builder.Services.AddDbContext<DataContext>(options =>
+//{
+//    options.UseSqlServer(connectionString, b => b.MigrationsAssembly("CQRS_MediatoR_CRUD"));
+//});
 
 var app = builder.Build();
 
